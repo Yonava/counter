b5094ee9404d32be06b11d12c1f5480ea3fb16cd
yonava <yonavossandreae@gmail.com>
2025-10-25 17:16:35 -0400

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import YonaCounter from './YonaCounter.vue';
+
+const total = ref(0)
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +11,8 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  {{ total }}
+  <YonaCounter @counter-update="(e) => total += e" />
 </template>
 
 <style scoped></style>
---
+++ src/YonaCounter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const counter = ref(0)
+
+const emits = defineEmits<{
+  (e: 'counter-update', count: number): void
+}>()
+
+watch(counter, (v) => emits('counter-update', v))
+</script>
+
+<template>
+  <h1>
+    {{ counter }}
+  </h1>
+  <button @click="counter++">
+    increment
+  </button>
+  <button @click="counter--">
+    decrement
+  </button>
+</template>
